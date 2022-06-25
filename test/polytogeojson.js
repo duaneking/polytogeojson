94b0d6100c08caaf82addc456e96179ecd31a769
Duane King <duaneking@users.noreply.github.com>
2022-06-25 12:04:08 -0700

    add NA test and data files as test

--- test/polytogeojson.js
+++ test/polytogeojson.js
@@ -7,12 +7,16 @@ describe('#polytogeojson', function () {
     it('austria.poly', function () {
         test('test/data/austria.poly', 'test/data/austria.geojson');
     });
+    it('north-america.poly', function () {
+        test('test/data/north-america.poly', 'test/data/north-america.geojson');
+    });
     it('complex.poly', function () {
         test('test/data/complex.poly', 'test/data/complex.geojson');
     });
 });
 
 function test(actual, expected) {
-    assert.deepEqual(polytogeojson(fs.readFileSync(actual, { encoding: 'utf8' })),
-                     JSON.parse(fs.readFileSync(expected)));
+    var poly = polytogeojson(fs.readFileSync(actual, { encoding: 'utf8' }));
+		
+    assert.deepEqual(poly, JSON.parse(fs.readFileSync(expected)));
 }
